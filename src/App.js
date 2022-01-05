c1fae85f669e5cb63044633b3f814026cc41a027
Sarah Majeed <sarahmajeed@Sarahs-MacBook-Air.local>
2022-01-05 18:29:18 +0500

    Creating chain on indura and integrating the dsc contract

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import './App.css';
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "./wallet/Connector";
-import web3 from "web3";
-import { useState } from "react";
+import Web3 from "web3";
+import { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
-import {Box} from '@mui/material';
+import { Box } from '@mui/material';
+import { abi } from './contract/dc.json';
 
 
 function App() {
   // web3-react hooks
   const { active, account, library, activate, deactivate } = useWeb3React();
   const [minting, setMinting] = useState(false);
+  const [balanceAmount, setbalanceAmount] = useState();
+
+  let web3 = new Web3(
+    "https://ropsten.infura.io/v3/1e744c5259c740ffb4a4a94b0ac330b8"
+  );
+
+  let dscContractAddress = "0xF6e13B49600DeF8cd5A1F90681b95Ce02Fb16821";
+
+  // initialize contract
+
+  async function getBalance() {
+    try {
+      if (account) {
+        let contract = new web3.eth.Contract( abi, dscContractAddress);
+
+        let balance = await contract.methods.balanceOf(account).call();
+        setbalanceAmount(balance);
+      }
+
+      console.log("BALANCE", balanceAmount);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   // Function to connect wallet
   async function connect() {
@@ -21,6 +46,7 @@ function App() {
   }
   }
 
+  
   async function mint() {
     setMinting(true);
     const myAccount = "0xb9eC43E9eFa48E4b9c3667E66732f521E1Bc633f"; //Account to receive payment
@@ -44,6 +70,15 @@ function App() {
       }
     });
   }
+
+  useEffect(() => {
+   
+    console.log("eth", account);
+    getBalance();
+  
+
+    // getAllAccounts();
+  }, [account]);
   
   
   return (
@@ -55,9 +90,34 @@ function App() {
       </Box>
       
       <Box>
-        {(active) ? <Button variant='contained' type="button" disabled={minting} onClick={mint} className="main-mint-btn">
+        {(active) ? <Box>
+          
+         
+          <Button variant='contained' type="button" disabled={minting} onClick={mint} className="main-mint-btn">
         {(minting) ? 'Waiting confirmation.' : 'Mint'}
-        </Button> : null}
+          </Button>
+          <Box>
+            <p label htmlFor="">Transfer eth from this account to another</p>
+            <p>
+              Balance of {account}: <b>{balanceAmount}</b>
+            </p>
+          </Box>
+        </Box> : null}
+        
+      </Box>
+
+      <Box>
+        {(active) ? <Box>
+          
+         
+          <Button variant='contained' type="button" disabled={minting} onClick={mint} className="main-mint-btn">
+        {(minting) ? 'Waiting confirmation.' : 'Transfer DSC'}
+          </Button>
+          <Box>
+            <p label htmlFor="">Transfer DSC from this account to another</p>
+            
+          </Box>
+        </Box> : null}
       </Box>
       
         
